1617312c568b42195a2d4b51dd4650c5697019c1
Cheng Vue <cvu3@ucsc.edu>
2019-01-24 02:56:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new]
-  get '/students/:id', to: 'students#show', as: 'student/new'
+  get '/students/:id', to: 'students#show', as: 'students/new'
 end
